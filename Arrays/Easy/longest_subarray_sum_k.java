4da3170e2ecd0d8bbd576199e90c553c2a53cf79
priyanshu-cloud <priyanshu.mahto7781@gmail.com>
2026-04-24 11:59:08 +0530

    update

--- Arrays/Easy/longest_subarray_sum_k.java
+++ Arrays/Easy/longest_subarray_sum_k.java
@@ -1,9 +1,9 @@
 public class longest_subarray_sum_k {
     public static void main(String[] args) {
         
-        int arr[] = {1,2,3,1,1,1,1,4,2,3};
-        int k = 3;
-        int len = 0;
+        // int arr[] = {1,2,3,1,1,1,1,4,2,3};
+        // int k = 6;
+        // int len = 0;
 
         //      ----------------->   Brute  [ TC - O(N^3)]
 
@@ -44,6 +44,39 @@ public class longest_subarray_sum_k {
          //    ----------------->  Better [ hashing ]
          // int arr[] = {1,2,3,1,1,1,1,4,2,3};
 
+
+
+         //    ----------------->  Optimal [ 2 pointer ]
+         // TC -> O(2N)
+         // SC -> O(1)
+
+         int arr[] = {1,2,3,1,1,1,1,1,3,3};
+         int k = 6;
+         int left = 0;
+         int right = 0;
+         int sum = arr[0];
+         int maxlen = 0;
+
+         while( right < arr.length){  // O(N)
+
+            while(left <= right && sum > k){ // O(N) --> Overall
+                sum -= arr[left]; // if sum > k then sub the array from left
+                left++; // shrink from left
+            }
+
+            if(sum == k){
+                maxlen = Math.max(maxlen, right - left +1); // update maxlen
+            }
+
+            right ++; // 1st move then add
+
+            if(right<arr.length){ // add under the boundary/array
+                sum += arr[right]; // sum = sum + arr[right]
+            }
+            
+         }
+
+         System.out.println(maxlen);
          
 
     }
