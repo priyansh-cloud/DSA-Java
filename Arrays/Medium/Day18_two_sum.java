f95033a49f39a004dc50733384833f231ec9d7a8
priyanshu-cloud <priyanshu.mahto7781@gmail.com>
2026-04-25 18:17:25 +0530

    Day 18: Two Sum problem solved

--- Arrays/Medium/Day18_two_sum.java
+++ Arrays/Medium/Day18_two_sum.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Day18_two_sum {
     public static void main(String[] args) {
         int arr[] = {2,6,5,8,11};
@@ -6,26 +7,38 @@ public class Day18_two_sum {
         int k = 14;
 
         // ----------------------> brute
-        for(int i = 0; i<arr.length; i++){ // O(n)
-            for(int j = i+1; j<arr.length; j++){ // O(n)
+        // for(int i = 0; i<arr.length; i++){ // O(n)
+        //     for(int j = i+1; j<arr.length; j++){ // O(n)
                 
-                if( arr[i] + arr[j] == k ){
-                    System.out.println(arr[i] + "," + arr[j]);
-                    break;
-                }
-            }
-        }
-
-        // ----------------------> better [ HashMap ]
-
-        // HashMap <Integer, Integer> map = new HashMap<>();
-
-        // for(int i = 0; i<arr.length; i++){
-        //     int a = book[i];
-        //     int more = k - a;
-        //     if
-
+        //         if( arr[i] + arr[j] == k ){
+        //             System.out.println(arr[i] + "," + arr[j]);
+        //             break;
+        //         }
+        //     }
         // }
+
+        // ----------------------> better [ HashMap ] TC -> O(N), SC -> O(N)
+
+        HashMap <Integer, Integer> map = new HashMap<>();
+        //      < need , i (index) >
+
+        for(int i = 0; i<arr.length; i++){
+            int need = k - arr[i];
+
+             // If need found, return indices
+            if( map.containsKey(need)){ // 1st check
+              
+                System.out.println(  map.get(need) + " " + i );
+                return;
+            }
+             // Store current element and index in hashmap
+            map.put(arr[i], i); // then move the elements into the map 
+            
+        }
+        System.out.println("not found");
+
+
+    
 
         
     }
