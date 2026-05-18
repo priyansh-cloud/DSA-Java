b1994ba1de2d1e8dd9e5f9b3596e07c25bf40cee
priyanshu-cloud <priyanshu.mahto7781@gmail.com>
2026-05-18 20:59:19 +0530

    Day 37

---
+++ Arrays/Hard/Day37_Count_Inversions.java
@@ -0,0 +1,27 @@
+public class Day37_Count_Inversions {
+    public static void main(String[] args) {
+        
+
+        //!             Brute
+
+        // int[] arr = {5, 4, 3, 2, 1};
+
+        // int count = 0;
+        // for(int i = 0; i<arr.length; i++ ){
+
+        //     for(int j = i + 1; j<arr.length; j++ ){
+        //         if( arr[i] > arr[j] ){
+        //             count++;
+        //         }
+                
+
+        //     }
+        // }
+        // System.out.println(count);
+
+
+        //!              Optimal [ Merge Sort ]
+
+        
+    }
+}
