f6c51c8290ebe6410317fbda0050c06faf1ff221
priyanshu-cloud <priyanshu.mahto7781@gmail.com>
2026-05-22 16:55:28 +0530

    Day 43

--- Binary_Search/Day43_Floor_and_Ceil_in_Sorted_Array.java
+++ Binary_Search/Day43_Floor_and_Ceil_in_Sorted_Array.java
@@ -4,5 +4,45 @@ public class Day43_Floor_and_Ceil_in_Sorted_Array {
     
     public static void main(String[] args) {
         
+        int[] arr = {3, 4, 4, 7, 8, 10};
+        int x = 5;
+        int low = 0;
+        int high = arr.length-1;
+        int floor = -1;
+
+        while( low <= high ){
+            int mid = ( low + high )/2;
+
+            // Floor
+            if( arr[mid] <= x ){
+                floor = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        
+
+        // reset
+        low = 0;
+        high = arr.length - 1;
+
+        // Ceil
+        int ceil = -1;
+        while( low <= high ){
+            int mid = ( low + high )/2;
+
+            if( arr[mid] >= x ){
+                ceil = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        System.out.println("Floor : " + arr[floor]);
+        System.out.println("Ceil : " + arr[ceil]);
     }
 }
