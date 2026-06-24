02c5288b3edb90dfb29dd591ee5330f6b76077c1
priyanshu-cloud <priyanshu.mahto7781@gmail.com>
2026-06-24 17:48:26 +0530

    updated

--- BasicRecursion.java/NumberExample_Recursion.java
+++ BasicRecursion.java/NumberExample_Recursion.java
@@ -11,12 +11,14 @@ public class NumberExample_Recursion {
     static void print( int n ){
 
         if( n == 5 ){
-            
+
             System.out.println(5); 
             return;
         }
         System.out.println(n); 
 
+        // Tail recursion
+        // This is the last function call
         print( n + 1 );
     }
 
